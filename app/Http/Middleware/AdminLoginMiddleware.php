68a9202e95f62c0278c1a38497072c5c9cff6a1c
ThuyDungg <thuydungcnptit@gmail.com>
2018-09-05 16:59:23 +0700

    role

---
+++ app/Http/Middleware/AdminLoginMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+class AdminLoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user->role == 1) {
+                return $next($request);
+            }
+            else {
+                return redirect('/logout');
+            }
+        }
+        else {
+            return redirect('/login');
+        }
+    }
+}
